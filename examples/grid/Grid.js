f72ece51c295fa61d5a5991bc5e8e113584d5588
Gerjo <gerjoo@gmail.com>
2013-10-07 22:02:25 +0200

    New demo showing a grid.

---
+++ examples/grid/Grid.js
@@ -0,0 +1,78 @@
+
+Grid.prototype = new Game();
+
+function Grid(container) {
+    // Call super class' constructor:
+    Game.apply(this, arguments);
+    
+    this.stats.show(true);
+    this.stats.setColor("black");
+    this.setFps(30);
+}
+
+Grid.prototype.draw = function(renderer) {
+    renderer.clear();
+        
+    var color   = "rgba(0, 0, 0, 0.2)";
+    var spacing = 40;
+    var font    = "bold 11px monospace";
+    var fontColor = "black";
+    
+    
+    //string, x, y, color, align, valign, font
+    
+    renderer.begin();
+    for(var x = 0; x < this.hw; x += spacing) {
+        renderer.line(x, -this.hh, x, this.hh);
+        renderer.text(x, x, 0, fontColor, "center", "middle", font);
+    }
+    
+    for(var x = 0; x > -this.hw; x -= spacing) {
+        renderer.line(x, -this.hh, x, this.hh);
+        renderer.text(x, x, 0, fontColor, "center", "middle", font);
+    }
+    
+    for(var y = 0; y < this.hh; y += spacing) {
+        renderer.line(-this.hw, y, this.hw, y);
+        renderer.text(y, 0, y, fontColor, "center", "middle", font);
+    }
+    
+    for(var y = 0; y > -this.hh; y -= spacing) {
+        renderer.line(-this.hw, y, this.hw, y);
+        renderer.text(y, 0, y, fontColor, "center", "middle", font);
+    }
+    
+    renderer.circle(0, 0, 10);
+    renderer.stroke(color);
+    
+    
+    
+    renderer.begin();
+    renderer.arrow(0, 0, this.input.x, this.input.y);
+    renderer.stroke("red");
+    
+    renderer.begin();
+    renderer.line(this.input.x, this.input.y, this.input.x, 0);
+    renderer.line(this.input.x, this.input.y, 0, this.input.y);
+    renderer.stroke("rgba(10, 255, 10, 1)");
+    
+    renderer.text(this.input.x + " x " + this.input.y, this.input.x, this.input.y, "black", "center", "bottom");
+    
+    
+    var v = Vector.CreateAngular(-Math.QuarterPI).trim(220);
+    renderer.begin();
+    renderer.vector(v);
+    renderer.stroke("hotpink");
+    renderer.text("π/-4", v.x, v.y);
+    
+    var v = Vector.CreateAngular(Math.QuarterPI).trim(220);
+    renderer.begin();
+    renderer.vector(v);
+    renderer.stroke("hotpink");
+    renderer.text("π/4", v.x, v.y);
+};
+
+
+Grid.prototype.update = function(dt) {
+    
+};
