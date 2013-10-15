86f0a77b236d7ca242855babb1749ad4e2502d53
Gerjo <gerjoo@gmail.com>
2013-10-15 17:32:31 +0200

    More random helpers.

--- src/aux/Random.js
+++ src/aux/Random.js
@@ -20,8 +20,52 @@ Random = (function() {
             return mt.nextBoolean();
         },
         
+        /// Returns a floating point.
         Range: function(min, max) {
             return mt.nextFloat() * (max - min) + min;
+        },
+        
+        /// Array of floating points
+        FloatArray: function(size) {
+            var r = new Array(size);
+            
+            while(size--) {
+                r[size] = mt.nextFloat();
+            }
+            
+            return r;
+        },
+        
+        /// Array of integers:
+        IntegerArray: function(size) {
+            var r = new Array(size);
+            
+            while(size--) {
+                r[size] = mt.nextInteger();
+            }
+            
+            return r;
+        },
+        
+        /// Array of booleans:
+        BooleanArray: function(size) {
+            var r = new Array(size);
+            
+            while(size--) {
+                r[size] = mt.nextBoolean();
+            }
+            
+            return r;
+        },
+        
+        RangeArray: function(size, min, max) {
+            var r = new Array(size);
+            
+            while(size--) {
+                r[size] = mt.nextFloat() * (max - min) + min;
+            }
+            
+            return r;
         },
         
         /// Returns a random vector distributed on a unit circle.
