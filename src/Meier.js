c87091afa6a37aee905367c23635204408483e2a
Gerjo <gerjoo@gmail.com>
2013-10-15 13:42:51 +0200

    Some documentation.

--- src/Meier.js
+++ src/Meier.js
@@ -36,20 +36,41 @@ var LoadEngine = function(prefix) {
 };
 
 
-
+/// Simple file loading facility. This is designed to eliminate
+/// the need for maintaining a bunch "script" tags.
+///
+/// Engine code can be configured to be loaded from a specific
+/// folder or URL. E.g., calling:
+///    Meier("www.example.com");
+///    Meier.Include("meier/engine/Game");
+/// Will attempt to load 'www.example.com/engine/Game.js'. 
+///
+/// Non-engine code will be loaded as-is. E.g.,
+///   Meier("www.example.com");
+///   Meier.Include("./MyGame.js");
+///
+/// Will attempt to load './MyGame.js' literally.
+///
+/// TODO: expand code to store files in LocalStorage for extremely
+/// fast loading. Though this require versioning. 
+///
 var Meier = (function() {
     var enginePrefix = "";
     var ident        = "meier/";
     var extension    = ".js";
     var loaded       = {};
     
+    // Actors like a C++ functor, sets up the engine file prefix.
     var exposed = function(prefix) {
-        enginePrefix = prefix;
+        enginePrefix = prefix || "";
     }
     
+    // Load files directly:
     exposed.Include = function(file) {
         var normalized = file;
         
+        // The ".js" file extension is optional. Append it here
+        // if not found.
         if(normalized.substr(-extension.length) != extension) {
             normalized += extension;
         }
@@ -57,16 +78,19 @@ var Meier = (function() {
         // Prefix the engine code (e.g., load from a different URL)
         if(normalized.substr(0, ident.length) == ident) {
             normalized = enginePrefix + normalized.substring(ident.length);
+        
         } else {
-            //normalized = normalized;
+            // TODO: possibly prefix game code, too?
+            //normalized = someprefix + normalized;
         }
         
-        if( ! loaded[normalized] ) {
+        // Make sure this file isn't loaded already:
+        if( ! loaded[normalized.toLowerCase()] ) {
             console.log("Loading: " + normalized);
             
-            loaded[normalized] = true;
+            loaded[normalized.toLowerCase()] = true;
             
-            // Load the file, directly, blocking. 
+            // Load the file, directly, blocking.
             document.writeln('<scri' + 'pt src="' + normalized + '" type="text/javascript"></scri' + 'pt>'); 
             
         } else {
@@ -74,6 +98,7 @@ var Meier = (function() {
         }
     }
     
+    // Return the publically available logic:
     return exposed;
 }());
 
