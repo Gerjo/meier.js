24b4770678b1b868fdcb898fea388ac1592a1dcc
Gerjo <gerjoo@gmail.com>
2013-10-24 22:51:07 +0200

    Two renames.

--- src/math/Line.js
+++ src/math/Line.js
@@ -1,44 +1,43 @@
 define(function(require) {
     var Vector = require("meier/math/Vector");
-    var Point  = Vector;
     
     /// Accepts:
     /// [Vector, Vector]
     /// [number, number, number, number]
-    function LineSegment(a, b, c, d) {
+    function MeierLineSegment(a, b, c, d) {
     
         if(a instanceof Vector) {
             this.a = a;
             this.b = b;
         } else {
-            this.a = new Point(a, b);
-            this.b = new Point(c, d);
+            this.a = new Vector(a, b);
+            this.b = new Vector(c, d);
         }
     }
 
     /// Compute the angle between two lines:
-    LineSegment.prototype.angleBetween = function(other) {
+    MeierLineSegment.prototype.angleBetween = function(other) {
         // TODO: can we optimize this?
         return this.direction().angleBetween(other.direction);
     };
 
     /// Compute the angle of direction:
-    LineSegment.prototype.angle = function() {
+    MeierLineSegment.prototype.angle = function() {
         return this.direction().angle();
     };
 
-    LineSegment.prototype.middle = function() {
+    MeierLineSegment.prototype.middle = function() {
         return new Vector(
             LerpFloat(this.a.x, this.b.x, 0.5),
             LerpFloat(this.a.y, this.b.y, 0.5)
         );
     };
 
-    LineSegment.prototype.clone = function() {
-        return new LineSegment(this.a.x, this.a.y, this.b.x, this.b.y);
+    MeierLineSegment.prototype.clone = function() {
+        return new MeierLineSegment(this.a.x, this.a.y, this.b.x, this.b.y);
     };
 
-    LineSegment.prototype.direction = function() {
+    MeierLineSegment.prototype.direction = function() {
         return new Vector(
             this.b.x - this.a.x,
             this.b.y - this.a.y 
@@ -47,26 +46,26 @@ define(function(require) {
 
     /// Horizontal line's slope is 0
     /// Vertical line's slope is Infinity (that's what javscript comes up with)
-    LineSegment.prototype.slope = function() {
+    MeierLineSegment.prototype.slope = function() {
         var d = this.direction();
         return d.y / d.x;
     };
 
-    LineSegment.prototype.lengthSQ = function() {
+    MeierLineSegment.prototype.lengthSQ = function() {
         return Math.pow(this.b.x - this.a.x, 2) + Math.pow(this.b.y - this.a.y, 2);
     };
 
-    LineSegment.prototype.length = function() {
+    MeierLineSegment.prototype.length = function() {
         return Math.sqrt(Math.pow(this.b.x - this.a.x, 2) + Math.pow(this.b.y - this.a.y, 2));
     };
 
-    LineSegment.prototype.project = function(axis) {
-        return new LineSegment(
+    MeierLineSegment.prototype.project = function(axis) {
+        return new MeierLineSegment(
             this.a.project(axis),
             this.b.project(axis)
         );
     };
 
-return LineSegment;
+return MeierLineSegment;
 
 });
