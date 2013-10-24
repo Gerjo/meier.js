3402e23e4b34fcd4dc35a1e4f8ad73e2b503baf5
Gerjo <gerjoo@gmail.com>
2013-10-24 22:49:55 +0200

    Private vars prefixed with an underscore.

--- src/engine/Game.js
+++ src/engine/Game.js
@@ -29,48 +29,48 @@ define(function(require) {
         this.isSlow   = false;
     
         // Update loop related matter:
-        this.frame            = 0;               // frame counter.
-        this.clock            = new Stopwatch(); // Wall Clock.
-        this.fps              = 15;              // Desired framerate
-        this.timer            = new Stopwatch(); // Delta time counter.
-        this.width            = container.offsetWidth;
-        this.height           = container.offsetHeight;
-        this.hw               = this.width  * 0.5;
-        this.hh               = this.height * 0.5;    
+        this._frameCounter   = 0;               // frame counter.
+        this.clock           = new Stopwatch(); // Wall Clock.
+        this._fps            = 15;              // Desired framerate
+        this._dttimer        = new Stopwatch(); // Delta time counter.
+        this.width           = container.offsetWidth;
+        this.height          = container.offsetHeight;
+        this.hw              = this.width  * 0.5;
+        this.hh              = this.height * 0.5;    
         
-        this.renderer         = new Renderer(container, this.width, this.height);
+        this._renderer       = new Renderer(container, this.width, this.height);
     
         // Debug information:
         this.log             = new Logger(this.width, this.height);
         this.stats           = this.log; // Legacy support.
     
         // Keyboard, touch and mouse events:
-        this.input           = new Input(this.renderer.canvas, this.width, this.height, this.isTablet);
+        this.input           = new Input(this._renderer.canvas, this.width, this.height, this.isTablet);
     
         // Default loop:
-        this.intervalId      = setInterval(this._update.bind(this), 1000 / this.fps);
+        this._intervalId     = setInterval(this._update.bind(this), 1000 / this._fps);
     }
     
     Game.prototype.setFps = function(fps) {
-        this.fps = fps;
+        this._fps = fps;
     
         // Remove current loop:
-        clearTimeout(this.intervalId);
+        clearTimeout(this._intervalId);
     
         // Scedule a new loop:
-        this.intervalId = setInterval(this._update.bind(this), 1000 / this.fps);
+        this._intervalId = setInterval(this._update.bind(this), 1000 / this._fps);
     };
 
     Game.prototype._update = function() {
-        var dt = this.timer.peek() * 0.001;
-        this.timer.start();
+        var dt = this._dttimer.peek() * 0.001;
+        this._dttimer.start();
     
         // Clamp dt:
         if(dt > 0.2) {
             dt = 0.2;
         }
     
-        this.log.log("FPS", Math.ceil(1 / dt) + "/" + this.fps);
+        this.log.log("FPS", Math.ceil(1 / dt) + "/" + this._fps);
         this.log.log("Clock", Math.floor(this.clock.peek() * 0.001));
     
         // User defined update.
@@ -81,22 +81,22 @@ define(function(require) {
         /// a b 0
         /// c d 0
         /// e f 1
-        this.renderer.context.setTransform(
+        this._renderer.context.setTransform(
             1,  0, 
             0,  1, 
         
             // Draw from center. A half pixel offset 
             // gives sharper lines.
-            this.renderer.hw + 0.5,
-            this.renderer.hh + 0.5);
+            this._renderer.hw + 0.5,
+            this._renderer.hh + 0.5);
     
         // User defined draw loop:
-        this.draw(this.renderer);
+        this.draw(this._renderer);
     
         // Update statistics (TODO: rework some things)
         this.log.update();
     
-        this.log.draw(this.renderer);
+        this.log.draw(this._renderer);
     };
 
     Game.prototype.update = function(dt) {
