ba019dc64bc01bdd2631c0cca1e0acd45d2ddad0
Gerjo <gerjoo@gmail.com>
2013-10-24 23:50:41 +0200

    Testing how these tests work.

---
+++ test/test-test.js
@@ -0,0 +1,12 @@
+// http://testanything.org/wiki/index.php/Main_Page
+// https://github.com/substack/mocha-testling-ci-example/blob/master/test/beep.js
+//
+
+// Ok... let's test this thing here - 
+console.log("ok 1 - donuts are nice.");
+console.log("ok 2 - chocolate chip cookies are lovely")
+console.log("not ok 3 - i ran out of cookies");
+console.log("---");
+console.log("   message: THIS IS DETRIMENTAL");
+console.log("   severity: fatal");
+console.log("ok 4 nom nom nom nom");
