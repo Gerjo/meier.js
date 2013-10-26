2bd18f6f78413e322526762915ae8b2aaf47a005
Gerjo <gerjoo@gmail.com>
2013-10-26 13:26:58 +0200

    New attempt at this testing framework.

--- test/test-test.js
+++ test/test-test.js
@@ -3,10 +3,13 @@
 //
 
 // Ok... let's test this thing here - 
-console.log("ok 1 - donuts are nice.");
+console.log("TAP version 13");
+console.log("1..4");
+console.log("ok 1 - donuts are nice");
 console.log("ok 2 - chocolate chip cookies are lovely");
 /*console.log("not ok 3 - i ran out of cookies");
 console.log("---");
 console.log("   message: THIS IS DETRIMENTAL");
 console.log("   severity: fatal");*/
-console.log("ok 4 nom nom nom nom");
+console.log("ok 3 - foo foo bar");
+console.log("ok 4 - nom nom nom nom");
