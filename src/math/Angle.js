f7792b70efc5ad85bc2e77ca1cd876de6dfd722e
Gerjo <gerjoo@gmail.com>
2013-10-26 12:56:42 +0200

    Angle module.

--- src/math/Angle.js
+++ src/math/Angle.js
@@ -1,78 +1,84 @@
-/// From degrees to radians.
-function ToRadians(degrees) {
-    return degrees * Math.PI / 180;
-}
-
-/// From radians to degrees.
-function ToDegrees(radians) {
-    return 180 / Math.PI * radians;
-}
-
-/// Radians to multiples of pi.
-function ToPiMultiples(radians) {
-    return radians / Math.PI;
-}
-
-/// Map a [0..PI, -PI..0] range to [0...2PI]
-function ToAbsoluteRadians(a) {
-    if(a < 0) {
-        return Math.PI + (Math.PI + a);
-    }
+define(function(require) {
     
-    return a;
-}
+    return {
+        /// From degrees to radians.
+        ToRadians: function(degrees) {
+            return degrees * Math.PI / 180;
+        },
 
-function DeltaRelativeRadians(a, b) {
-    var delta = b - a;
-    return delta + ((delta > Math.PI) ? -Math.TwoPI : (delta < -Math.PI) ? Math.TwoPI : 0)
-}
+        /// From radians to degrees.
+        ToDegrees: function(radians) {
+            return 180 / Math.PI * radians;
+        },
 
-/// Map [-nPI...nPI] to [0..PI, -PI..0] range
-function ToRelativeRadians(a) {
+        /// Radians to multiples of pi.
+        ToPiMultiples:function(radians) {
+            return radians / Math.PI;
+        },
+
+        /// Map a [0..PI, -PI..0] range to [0...2PI]
+        ToAbsoluteRadians:function(a) {
+            if(a < 0) {
+                return Math.PI + (Math.PI + a);
+            }
     
-    // Normalize to something positive:
-    while(a < 0) {
-        a += Math.TwoPI;
-    }
+            return a;
+        },
+
+        DeltaRelativeRadians: function(a, b) {
+            var delta = b - a;
+            return delta + ((delta > Math.PI) ? -Math.TwoPI : (delta < -Math.PI) ? Math.TwoPI : 0)
+        },
+
+        /// Map [-nPI...nPI] to [0..PI, -PI..0] range
+        ToRelativeRadians: function(a) {
     
-    // Normalize to [0...2PI]:
-    while(a >= Math.TwoPI) {
-        a -= Math.TwoPI;
-    }
+            // Normalize to something positive:
+            while(a < 0) {
+                a += Math.TwoPI;
+            }
     
-    // Normalize [PI...2PI] to [-PI..0]
-    if(a > Math.PI) {
-        return (a - Math.PI) - Math.PI;
-    }
+            // Normalize to [0...2PI]:
+            while(a >= Math.TwoPI) {
+                a -= Math.TwoPI;
+            }
     
-    // Range good as-is.
-    return a;
-}
+            // Normalize [PI...2PI] to [-PI..0]
+            if(a > Math.PI) {
+                return (a - Math.PI) - Math.PI;
+            }
+    
+            // Range good as-is.
+            return a;
+        },
 
 
-/// Radians to degrees and Math.round.
-function ToDegreesRounded(radians) {
-    return Math.round(180 / Math.PI * radians);
-}
+        /// Radians to degrees and Math.round.
+        ToDegreesRounded: function(radians) {
+            return Math.round(180 / Math.PI * radians);
+        },
 
 
-/// Determine if an angle lies between two other angles. This
-/// assumes the angles are given in atan2 format.
-///
-/// @todo better efficency on the inner workings.
-///
-/// @param {angle} The angle to test for.
-/// @param {a} The lower (or upper) bound
-/// @param {b} The upper (or lower) bound
-/// @return boolean indicating whether angle lies between angles.
-function IsAngleInBetween(angle, a, b) {
+        /// Determine if an angle lies between two other angles. This
+        /// assumes the angles are given in atan2 format.
+        ///
+        /// @todo better efficency on the inner workings.
+        ///
+        /// @param {angle} The angle to test for.
+        /// @param {a} The lower (or upper) bound
+        /// @param {b} The upper (or lower) bound
+        /// @return boolean indicating whether angle lies between angles.
+        IsAngleInBetween: function(angle, a, b) {
     
-    a     = Vector.CreateAngular(a);
-    b     = Vector.CreateAngular(b);
-    angle = Vector.CreateAngular(angle);
+            a     = Vector.CreateAngular(a);
+            b     = Vector.CreateAngular(b);
+            angle = Vector.CreateAngular(angle);
     
-    return (
-        Math.sgn(a.cross(angle)) != Math.sgn(b.cross(angle))
-    );
-}
+            return (
+                Math.sgn(a.cross(angle)) != Math.sgn(b.cross(angle))
+            );
+        }
+        
+    }; // And return object.
+});
 
