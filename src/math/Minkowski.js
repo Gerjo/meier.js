5906402c856f426409e55d9495281e3cd6815670
Gerjo <gerjoo@gmail.com>
2013-10-29 20:13:13 +0100

    Added translation points.

--- src/math/Minkowski.js
+++ src/math/Minkowski.js
@@ -3,22 +3,17 @@ define(function(require) {
     var Hull   = require("meier/math/Hull");
     
     return {
-        ConvexSum: function(a, b, renderer) {
+        ConvexSum: function(translateA, a, translateB, b) {
             var r = [];
             
             a.forEach(function(p) {
                 
                 // Perhaps some filtering? We don't need every coordinate.
                 var sub = b.map(function(q) {
-                    return new Vector(q.x + p.x, q.y + p.y);
+                    return new Vector(
+                            q.x - p.x - translateA.x + translateB.x, 
+                            q.y - p.y - translateA.y + translateB.y);
                 });
-                
-                if(renderer) {
-                    renderer.begin();
-                    renderer.polygon(sub);
-                    renderer.stroke("black");
-                    renderer.fill("rgba(0, 0, 0, 0.2)");
-                }
                 
                 r.merge(sub);
             });
@@ -26,19 +21,7 @@ define(function(require) {
             // TODO: implement more efficient algorithm.
             var hull = Hull.GiftWrap(r);
             
-            if(renderer) {
-                renderer.begin();
-                renderer.polygon(hull);
-                renderer.stroke("black");
-                renderer.fill("rgba(0, 0, 0, 0.2)");
-            
-                renderer.begin();
-                renderer.polygon(a);
-                renderer.stroke("black");
-                renderer.fill("rgba(0, 0, 0, 0.2)");
-            }
-            
-            return r;
+            return hull;
         }
     };
     
