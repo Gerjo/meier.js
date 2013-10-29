284e77d5d40c54f411d4a2527dcab4acff6e1db3
Gerjo <gerjoo@gmail.com>
2013-10-29 20:13:34 +0100

    Gjk stub

---
+++ src/math/Gjk.js
@@ -0,0 +1,29 @@
+define(function(require) {
+    var Minkowski = require("meier/math/Minkowski");
+    var Vector    = require("meier/math/Vector");
+    var Polygon   = require("meier/math/Polygon");
+    var Hull      = require("meier/math/Hull");
+    
+    return {
+        Test: function(a, b, r) {
+            
+            //var t = a.vertices.map(function(p){ return new Vector(p.x + a.position.x, p.y + a.position.y); });
+            
+            var hull = Minkowski.ConvexSum(a.position, a.vertices, b.position, b.vertices);
+        
+            r.begin();
+            r.polygon(hull);
+            r.stroke("blue");
+            
+            r.begin();
+            r.polygon(a);
+            r.stroke("red");
+            
+            r.begin();
+            r.polygon(b);
+            r.stroke("green");
+        
+        }
+    };
+    
+});
