2087f8adb6c42fbe88e51a3d688cb2e1ec1142e1
Gerjo <gerjoo@gmail.com>
2013-10-31 17:36:51 +0100

    Pixel. Awesome for debugging.

---
+++ src/entities/Pixel.js
@@ -0,0 +1,20 @@
+define(function(require) {
+    var Entity = require("meier/engine/Entity");
+    
+    Pixel.prototype = new Entity();
+    function Pixel(x, y, color) {
+        Entity.call(this, x || 0, y || 0);
+        
+        this.color = color || "black";
+    }
+    
+    Pixel.prototype.draw = function(renderer) {
+        renderer.begin();
+        renderer.circle(0, 0, 2);
+        renderer.stroke(this.color);
+        
+        Entity.prototype.draw.call(this, renderer);
+    };
+    
+    return Pixel;
+});
