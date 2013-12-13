dd262745bc3ff1f504081ea1f734d020b336763a
Gerjo <gerjoo@gmail.com>
2013-12-13 19:50:43 +0100

    Removed second file. (one is useless without the other). Some hacks from game jam still present.

--- src/collections/CriterionTree.js
+++ src/collections/CriterionTree.js
@@ -4,6 +4,16 @@
 ///
 define(function(require) {
     
+    var Intersection = require("meier/math/Intersection");
+    var Rectangle    = require("meier/math/Rectangle");
+    var Heap         = require("meier/collections/Heap");
+    var LineSegment  = require("meier/math/Line");
+    var Vector       = require("meier/math/Vec")(2);
+    
+    // Tmp include:
+    var Types        = {}; Types.SOLID = 2;
+ 
+    
     var NodeIdCounter = 0;
     
     function Tree(w, h) {
@@ -61,6 +71,216 @@ define(function(require) {
         return out;
     };
     
+    /// Return actors that fit in a rectangle with a given tolerance. The
+    /// tolerance will only make the search area larger - not smaller. If
+    /// an actor is contained within the rectangle it shall always be
+    /// returned, no matter the tolerance.
+    Tree.prototype.getActorsFromRectangle = function(rectangle, tolerance) {
+    
+    
+        if( ! tolerance) {
+            // Determined by guessing.
+            //tolerance = Math.max(rectangle.width() * 0.1, 10);
+            tolerance = 10;
+        }
+    
+        var out    = {
+            actors: [],
+            rectangles: [],
+            lookup: []
+        };
+    
+        this.root.getActorsFromRectangle(out, rectangle, tolerance);
+    
+        delete out.lookup;
+    
+        return out; // out.entities
+    };
+    
+    Tree.prototype.canSeePoint = function(actor, point, isSolid, renderer) {
+        
+        var ehw = actor.width * 0.5;
+        var ehh = actor.height * 0.5;
+        
+        var volume = new Rectangle(
+            Math.min(actor.position.x - ehw, point.x),
+            Math.min(actor.position.y - ehh, point.y),
+            Math.max(actor.position.x + ehw, point.x),
+            Math.max(actor.position.y + ehh, point.y)
+        );
+    
+        var c        = actor.position;
+        var viewline = new LineSegment(c.x, c.y, point.x, point.y);
+    
+        // TODO: performance tweak "width".
+        var out      = this.getActorsFromRectangle(volume, 10);
+        var actors   = out.actors;
+    
+        
+        for(var i = 0; i < actors.length; ++i) {
+        
+            // TODO: proper solid flag.
+            // if(true || actors[i].type & solid) {
+            
+                if(renderer) {
+                    renderer.begin();
+                    renderer.line(viewline.a, viewline.b);
+                    renderer.rectangle(volume);
+                    renderer.fill("red");
+                }
+            
+            // Custom see-through callback function.
+            if(isSolid(actors[i])) {
+            
+                // Test against self:
+                if(actors[i] != actor) {
+                    var actorRect = actors[i].rectangle();
+                    
+                    // Coarse view testing:
+                    if(Intersection.Test.Rectangles(actorRect, volume)) { 
+                        // Accurate view testing.
+                        if(Intersection.Test.RectangleLineSegment(
+                            actorRect, viewline
+                        ) === true) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+
+        return true;
+    };
+    
+    Tree.prototype.findPath = function(a, b, context) {
+        //console.clear();
+        function h(nodeA, nodeB) {
+            return (
+                Math.abs(nodeA.x - nodeB.x)
+                +
+                Math.abs(nodeA.y - nodeB.y)
+            );
+        }
+    
+        var c = new Tree.Criterion();
+        c.minimalSize     = 30;
+        c.maximalSize     = 50;
+        c.minimalEntities = 0;
+        c.maximalEntities = 5;
+        c.solid           = Types.SOLID;
+    
+        var start = this.getNodeAt(c, a);
+        var end   = this.getNodeAt(c, b);
+    
+        // Try again, this time permit other entities present.
+        // in the odd event we get stuck in a wall.
+        if( ! start ) {
+            var cPermissive = new Tree.Criterion();
+            c.minimalSize     = 10;
+            c.maximalSize     = 30;
+            c.minimalEntities = 1;
+            c.maximalEntities = 10;
+            c.solid = Types.SOLID;
+        
+            start = this.getNodeAt(c, a);
+        }
+    
+        if( ! start || ! end) {
+            //console.log("Start or end unavailable. Start:" + start + " end:" + end);
+            return false;
+        }
+    
+        // Heap kindly taken from: https://github.com/qiao/PathFinding.js
+        var open = new Heap(function (a, b) {
+            return (lookup[a.id].f - lookup[b.id].f);
+        });
+    
+        var current, neighbours, neighbour, i, tentative;
+        var lookup = [];
+    
+        open.push(start);
+        lookup[start.id] = {};
+        lookup[start.id].h = h(start, end);
+        lookup[start.id].g = 0; // Step score.
+        lookup[start.id].f = lookup[start.id].h + lookup[start.id].g; // Sort score.
+        lookup[start.id].p = null; // terminate!
+        lookup[start.id].o = true;
+    
+        for(var t = 3000; t > 0 && ! open.empty(); --t) {
+            current = open.pop();
+            lookup[current.id].o = false;
+        
+            if(current == end) {
+                //console.log("Route found! [good news]");
+                break;
+            }
+        
+            neighbours = this.getNeighbours(c, current);
+            
+            //console.log(open.size());
+        
+            for(i = 0; i < neighbours.length; ++i) {
+                neighbour = neighbours[i];
+            
+                tentative = {};
+                tentative.h = h(neighbour, end);
+            
+                // TODO: a nicer step score? manhatten doesn't take 
+                // diagonal into account.
+                tentative.g = lookup[current.id].g + h(current, neighbour);
+            
+                tentative.f = tentative.g  + tentative.h * 1.1;
+                tentative.p = current;
+            
+            
+                if( ! lookup[neighbour.id] ) {
+                    tentative.o = true;
+                    lookup[neighbour.id] = tentative;
+                
+                    open.push(neighbour);
+                } else {
+                    if(lookup[neighbour.id].f > tentative.f) {
+                        lookup[neighbour.id] = tentative;
+                    
+                        // Additional test. This entry might've been popped before.
+                        if(tentative.o === true) { 
+                            open.updateItem(neighbour);
+                        }
+                    }
+                }
+            }
+        }
+    
+        if(current == end) {
+            var route = [], c;
+            var node = current;
+        
+            // Linkedlist unrolling:
+            while(node) {
+                c = new Vector(node.x, node.y);
+                route.push(c); 
+                node = lookup[node.id].p;
+            }
+        
+            // New start:
+            route.pop();
+            route.push(a.clone());
+        
+            route = route.reverse();
+        
+            // new end:
+            route.pop();
+            route.push(b.clone());
+        
+            //console.log("found. splendid");
+        
+            return route;    
+        }
+    
+        //console.log("Goal was not found.");
+        return false;
+    };
+    
     function Node(x, y, w, h) {
         this.id = ++NodeIdCounter;
         this.x  = x;
@@ -74,7 +294,54 @@ define(function(require) {
         this.right    = null;
         this.entities = [];
         this.hasDisbursed = false;
+        
+        this._rect = null;
     }
+    
+    Node.prototype.rectangle = function() {
+        return this._rect || (this._rect = new Rectangle(this.x - this.hw, this.y - this.hh, this.x + this.hw, this.y + this.hh));
+    };
+    
+    Node.prototype.getActorsFromRectangle = function(out, rectangle, tolerance) {
+        var Intersects = Intersection.Test.Rectangles;
+
+        // If this is empty, prune all children.
+        if(this.entities.length === 0) {
+            return;
+        }
+
+        // Future note: rather than recurse, if the entity count is small enough
+        // it's more sensible to hit-test directly. Say, when n < 4 or along
+        // those lines.
+
+        if(this.w > tolerance || this.h > tolerance) {
+            this.disburse();
+        }
+
+        // Has children, traverse when boundingbox fits:
+        if( ! this.isLeaf()) {
+            
+            if(Intersects(rectangle, this.left.rectangle()) === true) {
+                this.left.getActorsFromRectangle(out, rectangle, tolerance);
+            }
+        
+            if(Intersects(rectangle, this.right.rectangle()) === true) {
+                this.right.getActorsFromRectangle(out, rectangle, tolerance);
+            }
+    
+        // Leaf node, push entities.  
+        } else {
+        
+            for(var i = 0; i < this.entities.length; ++i) {
+                if( ! out.lookup[this.entities[i].hash] ) {
+                    out.lookup[this.entities[i].hash] = true;
+                    out.actors.push(this.entities[i]);
+                }
+            }
+        
+            out.rectangles.push(this.rectangle);
+        }
+    };
     
     Node.prototype.disburse = function() {
         if(this.hasDisbursed === true) {
@@ -103,23 +370,11 @@ define(function(require) {
         var ehw = entity.width  * 0.5;
         var ehh = entity.height * 0.5;
         
-        return (
-            ((this.x - this.hw < entity.position.x - ehw && this.x + this.hw > entity.position.x - ehw)
-            ||
-            (this.x - this.hw < entity.position.x + ehw && this.x + this.hw > entity.position.x + ehw))
-            &&
-            ((this.y - this.hh < entity.position.y - ehh && this.y + this.hh > entity.position.y - ehh)
-            ||
-            (this.y - this.hh < entity.position.y + ehh && this.y + this.hh > entity.position.y + ehh))
-        ) || (
-            ((entity.position.x - ehw < this.x - this.hw && entity.position.x + ehw > this.x - this.hw)
-            ||
-            (entity.position.x - ehw < this.x + this.hw && entity.position.x + ehw > this.x + this.hw))
-            &&
-            ((entity.position.y - ehw < this.y - this.hw && entity.position.y + ehw > this.y - this.hw)
-            ||
-            (entity.position.y - ehw < this.y + this.hw && entity.position.y + ehw > this.y + this.hw))
-        );
+        // TODO: inline this memory waste.
+        var a = new Rectangle(this.x - this.hw, this.y - this.hh, this.x + this.hw, this.y + this.hh);
+        var b = new Rectangle(entity.position.x - ehw, entity.position.y - ehh, entity.position.x + ehw, entity.position.y + ehh);
+        
+        return Intersection.Test.Rectangles(a, b);
     };
     
     Node.prototype.containsPoint = function(point) {
@@ -146,7 +401,7 @@ define(function(require) {
             ((this.y - this.hh <= node.y - node.hh && this.y + this.hh >= node.y - node.hh)
             ||
             (this.y - this.hh <= node.y + node.hh && this.y + this.hh >= node.y + node.hh))
-        )/* ||
+        ) ||
         // Not quite required unless the criterion varies from the one used to
         // obtain "node". TODO: inside test?
         (
@@ -157,7 +412,7 @@ define(function(require) {
             ((node.y - node.hh <= this.y - this.hh && node.y + node.hh >= this.y - this.hh)
             ||
             (node.y - node.hh <= this.y + this.hh && node.y + node.hh >= this.y + this.hh))
-        )*/;        
+        );      
     };
     
     Node.prototype.clear = function() {
@@ -176,6 +431,7 @@ define(function(require) {
     Node.prototype.add = function(entity) {
         
         if(this.contains(entity)) {
+            this.flag |= entity.type;
             this.entities.push(entity);
         }
     };
@@ -215,9 +471,9 @@ define(function(require) {
                 // good numEntities and width are in range.
             
                 // NOTE: untested (26th august)
-                //if(this.flag & heuristic.solid) {
-                //    return Criterion.Response.DISBURSE;
-                //}
+                if(this.flag & heuristic.solid) {
+                    return Criterion.Response.DISBURSE;
+                }
 
                 return Criterion.Response.PASS;
             } else if(width <= heuristic.minimalSize) {
@@ -246,9 +502,9 @@ define(function(require) {
                 }
             
                 // NOTE: untested (26th august)
-                //if(this.flag & heuristic.solids) {
-                //    return Criterion.Response.DISBURSE;
-                //}
+                if(this.flag & heuristic.solids) {
+                    return Criterion.Response.DISBURSE;
+                }
 
                 // Cannot shrink. Deal with it.
                 return Criterion.Response.PASS;
