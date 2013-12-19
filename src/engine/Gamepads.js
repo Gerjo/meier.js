9eb1fdc9ea4307c6a162a217c50a2fef68876e92
Gerjo <gerjoo@gmail.com>
2013-12-19 20:01:32 +0100

    Fixed "press once" feature.

--- src/engine/Gamepads.js
+++ src/engine/Gamepads.js
@@ -25,8 +25,25 @@ define(function(require) {
         
         var parent = this;
         
-        this.previous = {};
-        this.previous.a = function() { return 1 === hub._previous[index].buttons[0]; };
+        this.once = {};
+        this.once.a = function() { return 1 !== hub._previous[index].buttons[0] && 1 === hub._gamepads[index].buttons[0]; };
+        this.once.b = function() { return 1 !== hub._previous[index].buttons[1] && 1 === hub._gamepads[index].buttons[1]; };
+        this.once.x = function() { return 1 !== hub._previous[index].buttons[2] && 1 === hub._gamepads[index].buttons[2]; };
+        this.once.y = function() { return 1 !== hub._previous[index].buttons[3] && 1 === hub._gamepads[index].buttons[3]; };
+
+        this.once.select = function() { return 1 !== hub._previous[index].buttons[8] && 1 === hub._gamepads[index].buttons[8]; };
+        this.once.start  = function() { return 1 !== hub._previous[index].buttons[9] && 1 === hub._gamepads[index].buttons[9]; };
+
+        this.once.leftDown  = function() { return 1 !== hub._previous[index].buttons[10] && 1 === hub._gamepads[index].buttons[10]; };
+        this.once.rightDown = function() { return 1 !== hub._previous[index].buttons[11] && 1 === hub._gamepads[index].buttons[11]; };
+        
+        this.once.top    = function() { return 1 !== hub._previous[index].buttons[12] && 1 === hub._gamepads[index].buttons[12]; };
+        this.once.bottom = function() { return 1 !== hub._previous[index].buttons[13] && 1 === hub._gamepads[index].buttons[13]; };
+        this.once.left   = function() { return 1 !== hub._previous[index].buttons[14] && 1 === hub._gamepads[index].buttons[14]; };
+        this.once.right  = function() { return 1 !== hub._previous[index].buttons[15] && 1 === hub._gamepads[index].buttons[15]; };
+        
+        this.once.leftShoulder  = function() { return 1 !== hub._previous[index].buttons[4] && 1 === hub._gamepads[index].buttons[4]; };
+        this.once.rightShoulder = function() { return 1 !== hub._previous[index].buttons[5] && 1 === hub._gamepads[index].buttons[5]; };
     }
     
     Controller.prototype.connected = function() {
@@ -36,6 +53,7 @@ define(function(require) {
     Controller.prototype.index = function() {
         return this._index;
     };
+    
     /// Current state:
     Controller.prototype.a = function() { return 1 === this._hub._gamepads[this._index].buttons[0]; };
     Controller.prototype.b = function() { return 1 === this._hub._gamepads[this._index].buttons[1]; };
@@ -73,27 +91,6 @@ define(function(require) {
     };
     
     
-    /// Current state:
-    Controller.prototype.a = function() { return 1 === this._hub._gamepads[this._index].buttons[0]; };
-    Controller.prototype.b = function() { return 1 === this._hub._gamepads[this._index].buttons[1]; };
-    Controller.prototype.x = function() { return 1 === this._hub._gamepads[this._index].buttons[2]; };
-    Controller.prototype.y = function() { return 1 === this._hub._gamepads[this._index].buttons[3]; };
-    
-    Controller.prototype.select = function() { return 1 === this._hub._gamepads[this._index].buttons[8]; };
-    Controller.prototype.start  = function() { return 1 === this._hub._gamepads[this._index].buttons[9]; };
-    
-    Controller.prototype.leftDown  = function() { return 1 === this._hub._gamepads[this._index].buttons[10]; };
-    Controller.prototype.rightDown = function() { return 1 === this._hub._gamepads[this._index].buttons[11]; };
-    
-    Controller.prototype.top    = function() { return 1 === this._hub._gamepads[this._index].buttons[12]; };
-    Controller.prototype.bottom = function() { return 1 === this._hub._gamepads[this._index].buttons[13]; };
-    Controller.prototype.left   = function() { return 1 === this._hub._gamepads[this._index].buttons[14]; };
-    Controller.prototype.right  = function() { return 1 === this._hub._gamepads[this._index].buttons[15]; };
-    
-    Controller.prototype.leftShoulder  = function() { return 1 === this._hub._gamepads[this._index].buttons[4]; };
-    Controller.prototype.rightShoulder = function() { return 1 === this._hub._gamepads[this._index].buttons[5]; };
-    Controller.prototype.leftTrigger  = function() { return this._hub._gamepads[this._index].buttons[6]; };
-    Controller.prototype.rightTrigger = function() { return this._hub._gamepads[this._index].buttons[7]; };
     
     // Null object pattern, I suppose? The idea is that the end user doesn't
     // have to test if the controller is connected. It simply returns false
@@ -112,15 +109,13 @@ define(function(require) {
         this._gamepads    = [];
         this._previous    = []; // Previous gamepad state.
         this._controllers = [];
-        
-        this._dummy = new DummyInternalController();
-        
+                
         for(var i = 0; i < 4; ++i) {
             
             // Placeholder "empty" controller. Better than
             // continiously testing if controllers are connected.
-            this._gamepads.push(this._dummy);
-            this._previous.push(this._dummy);
+            this._gamepads.push(new DummyInternalController());
+            this._previous.push(new DummyInternalController());
             
             // Nothing detected per default:
             this._detected.push(false);
@@ -174,16 +169,22 @@ define(function(require) {
         for(var i = 0; i < 4; ++i) {
             
             if(pads[i]) {
+                ++this._numberConnected;
+                
                 
                 // Previous state
-                this._previous[i] = this._gamepads[i];
+                this._previous[i].axes = this._gamepads[i].axes;
+                this._previous[i].buttons = this._gamepads[i].buttons;
          
-                // New state
-                this._gamepads[i] = pads[i];
+         
+                // Make a deep copy of the new state
+                this._gamepads[i].axes = pads[i].axes.clone();
+                this._gamepads[i].buttons = pads[i].buttons.clone();
                 
                 if(this._detected[i] === false) {
                     this._detected[i] = true;
                     this._controllers[i]._isConnected = true;  
+                    
                     // Call user defined event:
                     this.onConnect(this._controllers[i]);
                 }
@@ -193,15 +194,11 @@ define(function(require) {
                 this._controllers[i]._isConnected = false;
                 
                 // Assign a dummy gamepad
-                this._previous[i] = this._dummy;
-                this._gamepads[i] = this._dummy;
+                this._previous[i] = new DummyInternalController();
+                this._gamepads[i] = new DummyInternalController();
                 
                 // Call user defined event
                 this.onDisconnect(this._controllers[i]);                
-            }
-            
-            if(pads[i]) {
-                ++this._numberConnected;
             }
         }
     };
