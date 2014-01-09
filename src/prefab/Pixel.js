fb8b4fe0318ab57d8203932e5f6d7384f1728a96
Gerjo <gerjoo@gmail.com>
2014-01-09 12:13:06 +0100

    Renamed variables.

--- src/prefab/Pixel.js
+++ src/prefab/Pixel.js
@@ -9,17 +9,21 @@ define(function(require) {
     var Entity = require("meier/engine/Entity");
     
     Pixel.prototype = new Entity();
-    function Pixel(x, y, color) {
+    function Pixel(x, y, fill, stroke) {
         Entity.call(this, x || 0, y || 0, 2, 2);
         
-        this.color = color || "black";
-        
+        this.fill   = fill || "black";
+        this.stroke = stroke || null;
     }
     
     Pixel.prototype.draw = function(renderer) {
         renderer.begin();
         renderer.circle(0, 0, this.width);    
-        renderer.stroke(this.color);   
+        renderer.fill(this.fill); 
+        
+        if(this.stroke) {  
+            renderer.stroke(this.stroke);   
+        }
     };
     
     return Pixel;
