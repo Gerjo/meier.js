089ccede2caed77ce85326817289b8c803581a9c
Gerjo <gerjoo@gmail.com>
2014-01-30 23:57:29 +0100

    Random noise and notices.

--- src/aux/Hashing.js
+++ src/aux/Hashing.js
@@ -1,6 +1,15 @@
+/**
+ !* Part of meier.js - a game and math prototype library.
+ !*  Copyright (C) 2013 Gerard J. Meier <gerjoo@gmail.com>
+ !*
+ !*
+ !*/
+
+
+
 define(function(require) {
     var self = {
-        
+        // todo: entire file. do'h.
         
     }; // end self
     
