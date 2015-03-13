b051b54362305eae87c80c4d4071d010266f406a
Gerard <gerjoo@gmail.com>
2015-03-13 14:12:46 +0100

    unique id

--- examples/evermoar/Bucket.js
+++ examples/evermoar/Bucket.js
@@ -1,9 +1,12 @@
 define(function(require) {
 	var Vec2 = require("meier/math/Vec")(2);
 	
+	var NextID = 0;
+	
 	function Bucket(x, y, color) {
 		this.position = new Vec2(x || 0, y || 0);
-		this.color = color || "red";
+		this.color    = color || "red";
+		this.id       = NextID++;
 	}
 	
 	return Bucket;
