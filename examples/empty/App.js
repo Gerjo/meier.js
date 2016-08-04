cd6a3502408044548bba9251d02a6ab8910e5ede
Gerard <gerjoo@gmail.com>
2016-08-04 15:22:19 +0100

    Renamed ‘test’ to ‘app’. Moved to ‘empty’ folder.

--- examples/empty/App.js
+++ examples/empty/App.js
@@ -1,22 +1,22 @@
 define(function(require){
     var Game = require("meier/engine/Game");
     
-    Test.prototype = new Game();
+    App.prototype = new Game();
     
-    function Test(container) {        
+    function App(container) {        
         Game.call(this, container);
 
     }
     
-    Test.prototype.update = function(dt) {
+    App.prototype.update = function(dt) {
         Game.prototype.update.call(this, dt);
         
     };
     
-    Test.prototype.draw = function(renderer) {
+    App.prototype.draw = function(renderer) {
         Game.prototype.draw.call(this, renderer);
         
     }
     
-    return Test;
+    return App;
 });
