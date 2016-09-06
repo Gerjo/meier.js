5989f6f31e84b6552f8dd6d84c496d425ea32b99
Gerard <gerjoo@gmail.com>
2016-09-06 22:58:14 +0100

    Callable object.

--- src/engine/Document.js
+++ src/engine/Document.js
@@ -1,59 +1,59 @@
 define(function(require) {
 	
 	
-	var self; 
+	function self(selector) {
+		return self.Find(selector);
+	}; 
 	
-	self = {
-		Find: function(selector) {
+	self.Find = function(selector) {
 			if(selector instanceof HTMLElement) {
 				return selector;
 			}
 			
 			return document.querySelector(selector);
-		},
+	};
 		
-		Visible: function(e, isVisible) {
-			var e = self.Find(e);
-			
-			if(isVisible) {
-				e.style.display = "";
-			} else {
-				e.style.display = "none";
-			}
-			
-			return e;		
-		},
+	self.Visible = function(e, isVisible) {
+		var e = self.Find(e);
 		
-	    OnClick: function(selector, fn) {
-	        
-			var node = self.Find(selector);
-			
-	        var eventName = ("ontouchstart" in window) ? "touchend" : "click";
-        
-	        return node.addEventListener(eventName, fn, false);
-	    },
-		
-		Remove: function(selector) {
-			selector = self.Find(selector);
-			
-			if(selector.parentNode) {
-				selector.parentNode.removeChild(selector);
-				return true;
-			}
-			return false;
-		},
-		
-		Create: function(tag, className) {
-			var e = document.createElement(tag);
-			
-			if(className) {
-				e.className = className;
-			}
-			
-			return e;
+		if(isVisible) {
+			e.style.display = "";
+		} else {
+			e.style.display = "none";
 		}
+		
+		return e;		
 	};
 	
+    self.OnClick = function(selector, fn) {
+        
+		var node = self.Find(selector);
+		
+        var eventName = ("ontouchstart" in window) ? "touchend" : "click";
+    
+        return node.addEventListener(eventName, fn, false);
+    };
+	
+	self.Remove = function(selector) {
+		selector = self.Find(selector);
+		
+		if(selector.parentNode) {
+			selector.parentNode.removeChild(selector);
+			return true;
+		}
+		return false;
+	};
+	
+	self.Create = function(tag, className) {
+		var e = document.createElement(tag);
+		
+		if(className) {
+			e.className = className;
+		}
+		
+		return e;
+	};
+
 	
 	return self;
 	
