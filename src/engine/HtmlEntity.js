113e08436351bb22dd0ea4def15d0ccfffe4f8ec
Gerard <gerjoo@gmail.com>
2016-09-18 18:26:54 +0100

    Entity absolute positioning. General feature polish

--- src/engine/HtmlEntity.js
+++ src/engine/HtmlEntity.js
@@ -9,23 +9,59 @@
 define(function(require) {
 	var Entity = require("meier/engine/Entity");
 	var Doc    = require("meier/engine/Document");
+	var Vec2   = require("meier/math/Vec")(2);
+	
+	var Align = require("meier/engine/Enum")("left", "right", "center", "middle", "bottom", "top");
+	
+	
+	var LastUsedId = -1;
 	
 	HtmlEntity.prototype = new Entity();
 	function HtmlEntity(x, y, w, h) {
 		Entity.call(this, x || 0, y || 0, w || 0, h || 0);
 		
+		++LastUsedId;
 		
 		this._root = Doc.Create("div");
 		this._root.style.border = "1px solid red";
 		this._root.style.position = "absolute";
+		this._root.id = "meier_html_entity_container_" + LastUsedId;
+		
+		
+		this._hAlign = 0.5; // 0 = left, 1 = right
+		this._vAlign = 0.5; // 0 = top,  1 = bottom
 	}
+	
+	HtmlEntity.prototype._onAdd = function(game) {
+		Entity.prototype._onAdd.call(this, game);
+		
+		game.htmlContainer.appendChild(this._root);
+	};
+	
+	HtmlEntity.prototype.find = function(selector) {
+		return this._root.querySelector("#" + this._root.id + " " + selector);
+	};
+	
+	HtmlEntity.prototype.click = function(selector, fn) {
+		return Doc.OnClick(this.find(selector), function() {
+			fn(this);
+		});
+	};
+	
+	HtmlEntity.prototype.hover = function(selector, fn) {
+		return Doc.OnHover(this.find(selector), function() {
+			fn(this);
+		});
+	};
 	
 	HtmlEntity.prototype.html = function(html) {
 		this._root.innerHTML = html;
+		return this;
 	};
 	
 	HtmlEntity.prototype.text = function(text) {
 		this._root.innerText = text;
+		return this;
 	};
 	
 	HtmlEntity.prototype._onDelete = function() {
@@ -35,14 +71,28 @@ define(function(require) {
 		Entity.prototype._onDelete.call(this);
 	};
 	
-	HtmlEntity.prototype._onAdd = function(game) {
-		Entity.prototype._onAdd.call(this, game);
+	TODO("floating coordinates for Html Entities");
+	HtmlEntity.prototype.float = function(wherea, whereb) {
 		
-		game.htmlContainer.appendChild(this._root);
+		if(whereb) {
+			wherea = wherea.trim() + " " + whereb.trim();
+		}
+		
+		var ops = whereb.split(" ");
+		
+		for(var k in ops) {
+			
+			
+		}
+		
 	};
 	
 	HtmlEntity.prototype.update = function(dt) {
 		Entity.prototype.update.call(this, dt);
+		
+		
+		
+		//console.log(width, height);
 		
 		if(this.width == 0) {
 			this._root.style.width = "auto";
@@ -52,17 +102,39 @@ define(function(require) {
 			this._root.style.height = "auto";
 		}
 		
-		var x = this.position.x + this.game.hw;
-		var y = -this.position.y + this.game.hh;
+		// Computed size by HTML document
+		var width = this._root.offsetWidth;
+		var height = this._root.offsetHeight;
 		
-		this._root.style.left = x + "px";
-		this._root.style.top = y + "px";
 		
-		console.log(x, y);
+		
+		
+		// Bring to absolute game world space
+		var abs = this.toWorld();
+
+		// Center component in HTML space
+		abs.x -= width * 0.5;
+		abs.y += height * 0.5;
+
+
+		// Undo engine sub-pixel offset to promote disabled AA.
+		abs.x += 0.5;
+		abs.y -= 0.5;
+
+
+		// Bring to HTML space
+		abs.x += this.game.hw;
+		abs.y = -abs.y + this.game.hh;
+		
+		this._root.style.left = Math.round(abs.x) + "px";
+		this._root.style.top  = Math.round(abs.y) + "px";		
 	} 
 	
-	
-	
+	HtmlEntity.prototype.draw = function(renderer) {
+		renderer.begin();
+		renderer.rect(0, 0, 10, 10);
+		renderer.fill("red");
+	};
 	
 	return HtmlEntity;
 });
