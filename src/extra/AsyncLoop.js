85a12edd6f6bb6d2d9305fd04c1f31fca41dc579
Gerard <gerjoo@gmail.com>
2016-10-16 15:29:27 +0100

    Async loop. (experimental)

---
+++ src/extra/AsyncLoop.js
@@ -0,0 +1,64 @@
+define(function(require) {
+	
+	var Entity    = require("meier/engine/Entity");
+	var Stopwatch = require("meier/extra/Stopwatch");
+	
+	Task.prototype = new Entity();
+	
+	function Task(callable) {
+		Entity.call(this, 0, 0);
+		
+		this._timer = new Stopwatch();
+		this._iteration  = 0;
+		this._generation = 0;
+		this._callable = null;
+		
+		
+		if(callable) {
+			this.setTask(callable);
+		}
+	}
+	
+	Task.prototype.setTask = Task.prototype.set = function(callable) {
+		this._callable = callable;
+		this._iteration = 0;
+		this._generation = 0;
+	};
+	
+	Task.prototype.stop = function() {
+		this.setTask(null);
+		// perhaps cache, such that resume could work?
+	};
+	
+	Task.prototype.update = function(dt) {
+		
+		if(typeof this._callable == "function") {
+			++this._generation;
+			
+			var timer = this._timer;
+		
+			TODO("Tie async task / loop into FPS.");
+			
+			timer.start();
+		
+			var running = false;
+		
+			do {
+		
+				running = this._callable(this._iteration, this._generation);				
+				++this._iteration;
+
+			} while(running === true && timer.seconds() < 1/10);
+			
+			if(running == false) {
+				this._callable = null;
+				
+				console.log("Async Task completed.");
+			}
+			
+		}
+	};
+	
+	
+	return Task;
+});
